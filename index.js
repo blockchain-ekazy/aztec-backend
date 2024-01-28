cfc15cecc032ea6be53fab843fc893153a0dd1bf
arazxa <blockchain.ekazy@gmail.com>
2024-01-28 13:21:36 +0500

    first

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const routes = require("./routes/routes");
-const Moralis = require("moralis").default;
 
 require("dotenv").config();
 
 const app = express();
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    ssl: true,
-  })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Could not connect to MongoDB:", err));
-
-Moralis.start({
-  apiKey: "epklPSqwrWAsdinNNZ7A8DirMVDhpdb85l4DEaJbPzIxm2Wj2Ag1QGx6NqFMuO9P",
-});
 
 app.use(
   cors({
@@ -25,7 +12,6 @@ app.use(
   })
 );
 
-app.use(bodyParser.json());
 app.use("/api", routes);
 
 app.get("/", (req, res) => {
